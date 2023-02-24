dfb4f5a65dfa37e3de7e6f8cdea580d43771cc51
salmandhedhi7 <m.shahid.dhedhi@gmail.com>
2023-02-25 02:54:41 +0500

    my webpage commit

---
+++ 4.divide.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Enter Your First Number")
+
+let num2 = prompt("Enter Your Second Number")
+
+num1 = Number(num1)
+
+num2 = Number(num2)
+
+let num3 = num1 / num2
+
+alert ("Answer: " + num3)
+
